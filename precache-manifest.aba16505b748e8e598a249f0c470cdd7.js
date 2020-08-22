7975487beced4495d0c5030e3b1b9b7915a5316e
Svitlana Zaster <szaster@protonmail.com>
2020-08-22 16:12:54 -0500

    Updates

---
+++ precache-manifest.aba16505b748e8e598a249f0c470cdd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5059d824b5027d4f5d8ec0730bc13013",
+    "url": "/Pupster-App/index.html"
+  },
+  {
+    "revision": "4cbb1bc8bdc4e9cef696",
+    "url": "/Pupster-App/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "20f5fbaacac8b6dfde59",
+    "url": "/Pupster-App/static/css/main.7d516dac.chunk.css"
+  },
+  {
+    "revision": "4cbb1bc8bdc4e9cef696",
+    "url": "/Pupster-App/static/js/2.ba18e5e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pupster-App/static/js/2.ba18e5e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20f5fbaacac8b6dfde59",
+    "url": "/Pupster-App/static/js/main.a7145e38.chunk.js"
+  },
+  {
+    "revision": "aad6eae72743342810b1",
+    "url": "/Pupster-App/static/js/runtime-main.8642e48f.js"
+  }
+]);
