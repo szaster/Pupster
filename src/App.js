479be7227f838076bd9399815d4b61ee6ac3e21f
Svitlana Zaster <szaster@protonmail.com>
2020-08-22 17:58:23 -0500

    route bug fix

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ function App() {
     <Router>
       <div>
         <NavTabs />
-        <Route exact path="/Pupster-App" component={About} />
+        <Route path={`${process.env.PUBLIC_URL}/`} component={About} />
+        <Route exact path="/" component={About} />
         <Route exact path="/about" component={About} />
         {/* <Route exact path="/discover" component={Discover} /> */}
         <Route exact path="/search" component={Search} />
