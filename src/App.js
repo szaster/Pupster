9775a1ee885182a6c8662ad9d8856dcaf55e49fa
Svitlana Zaster <szaster@protonmail.com>
2020-08-22 16:25:14 -0500

    added gh-pages branch

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
 import About from "./pages/About";
-import Discover from "./pages/Discover";
+// import Discover from "./pages/Discover";
 import Search from "./pages/Search";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
         <NavTabs />
         <Route exact path="/" component={About} />
         <Route exact path="/about" component={About} />
-        <Route exact path="/discover" component={Discover} />
+        {/* <Route exact path="/discover" component={Discover} /> */}
         <Route exact path="/search" component={Search} />
       </div>
     </Router>
